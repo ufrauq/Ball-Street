2e9acba7d86b36adf5512a123e16b53f8797b11a
Zain Sirohey <zsirohey@uwo.ca>
2017-03-03 15:44:20 -0500

    Adding Comments

--- grails-app/controllers/cs2212/team5/UserAccountController.groovy
+++ grails-app/controllers/cs2212/team5/UserAccountController.groovy
@@ -17,18 +17,21 @@ class UserAccountController extends RestfulController {
     def createUser(){
         def userName = params.userName
         def password = params.password
+        //testing if user with userName exists
         def user = UserAccount.find{username == userName}
+
+        //if user with userName doesn't exist and the user supplied a password
         if(user == null && password != ""){
             def newData = new UserData();
             def global = League.find{name == "Global Leaderboard"}
             newData.addToLeagues(global)
             def newAccount = new UserAccount(username: userName, money: 100, netWorth: 100, mydata: newData, password: password).save()
-            global.numMembers = global.numMembers + 1
-            global.addToMembers(newAccount).save(flush: true)
-            response.status = 200;
+            global.numMembers = global.numMembers + 1 //increment members in global leaderboard
+            global.addToMembers(newAccount).save(flush: true) //add user to global leaderboard
+            response.status = 200; //success status
         }
         else
-            response.status = 502
+            response.status = 502 //failure status
     }
 
     /**
@@ -38,12 +41,12 @@ class UserAccountController extends RestfulController {
     def login(){
         def userName = params.userName
         def pass = params.password
-        def user = UserAccount.find{username == userName && password == pass}
-        if(user != null){
+        def user = UserAccount.find{username == userName && password == pass} //attempts to find user with userName and password
+        if(user != null){ //if user found, respond with user data
             respond user
         }
         else
-            response.status = 502
+            response.status = 502 //failure status
 
     }
 
